78e8c2b36deadbb986db8ad62e72910379e55fa2
Dakota States <dakota.states@gmail.com>
2020-03-10 00:34:07 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -5,12 +5,15 @@ def highest_student_gpa
 end
 
 def lowest_student_gpa
+  "SELECT min(gpa) from students;"
 end
 
 def average_student_gpa
+  "SELECT avg(gpa) from students;"
 end
 
 def total_tardies_for_all_students
+  "Select sum(tarties) from students"
 end
 
 def average_gpa_for_9th_grade
